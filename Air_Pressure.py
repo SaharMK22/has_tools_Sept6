0a9ba1f3fb178e0d1d1cd13b24ca34e4b2635bb2
SaharMK22 <mkarimi@arizona.edu>
2022-09-06 09:19:56 -0700

    SMK_Air_Pressure_Sept6

---
+++ Air_Pressure.py
@@ -0,0 +1,27 @@
+import math
+
+p0 = 101325         # reference pressure in pascals
+M = 0.02896968      # molr mass of air kg/mol
+g = 9.81            # gravity m/s2
+R0 = 8.314462618    # gas constant J/( mol.K)
+T = 273             # temp in Kelvin
+
+
+def air_pressure_at_height(h):
+    p0 = 101325         # reference pressure in pascals
+    M = 0.02896968      # molr mass of air kg/mol
+    g = 9.81            # gravity m/s2
+    R0 = 8.314462618    # gas constant J/( mol.K)
+    T = 273             # temp in Kelvin
+
+
+    ratio = -(g * h * M) / (R0 * T)
+    p_h = p0 * math.exp(ratio)
+    return p_h
+
+
+# List of elevations to run
+h_list = range(0, 1000, 100)
+for height in h_list:
+    p_h = air_pressure_at_height(height)
+    print(height, '  ', p_h)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # has_tools_Sept6
+
+This sample code just calculates the air pressure for a given elevation.
